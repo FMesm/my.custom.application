d79d3f83037b7246fd19086044c3618e8750e77e
François Mesmacque <francois.mesmacque@sapinnov.com>
2023-07-12 16:20:33 +0000

    Use library function

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,15 +1,21 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "umg/my/custom/library/util"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller, util) {
         "use strict";
 
         return Controller.extend("umg.my.custom.application.controller.View1", {
             onInit: function () {
 
+            },
+
+            onButtonPress: function (oEvent) {
+                // use function from the library
+                util.sayHello();
             }
         });
     });
